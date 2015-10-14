a44661c0cca7f3df9458181a668adbc96fa2281e
Jeffrey Arts <jeffrey@wearereasonablepeople.com>
2015-10-14 09:25:49 +0200

    Add support for getterWatch function

--- scripts/jss/modules/test.js
+++ scripts/jss/modules/test.js
@@ -17,14 +17,13 @@ JssService.addModule("test");
 
 Test.prototype.init = function(){
 
-    this.default = {
-        isValid: "false"
-    };
+    this.addData('isValid', false, {
+        getterWatchFunction: function(){console.log("Get this.test");},
+        setterWatchFunction: function(){console.log("Set this.test -> Hup Holland Hup");}
+    })
 
     this.getData('isValid',{
         fallback: ['ajax','cookie','attribute'],
-        getterWatchFunction: function(){console.log("Get this.test");},
-        setterWatchFunction: function(){console.log("Set this.test -> Hup Holland Hup");},
         ajax: {
             url: 'http://localhost:3000/api/users/241/oauth/token?access_token=afd69160535d79529885016f0cb143d9ac181b98839a4f549bb3aec96e1f31a5&nocache=1444802312863'
         }

34761c9c37daf5abea70069d2e434f771b5b707f
Jeffrey Arts <jeffrey@wearereasonablepeople.com>
2015-10-14 08:40:55 +0200

    Added UNTESTED setDataAjax funciton

--- scripts/jss/core/data/ajax.js
+++ scripts/jss/core/data/ajax.js
@@ -5,20 +5,24 @@
  *
  * 		| Options |
  * 		- url      {string}                                                     The request url
- * 		- jsonKey  {string}                                                     The target key, if nested use a dot notation
+ * 		- jsonKey  {string}                                                     The target key, if nested use a dot notation. If not set the attribute parameter will be used.
+ *
+ * @param {string} attribute                                                    The NAME of the inner variable (this.NAME), is also used as the key for the GET Request {NAME: value}
+ * @param {object} options                                                      Object with parameters used to make a GET request
+ * @return {string}                                                             The value of the given attribute, or false if an erro occured.
  */
-Jss.prototype.getDataAjax = function(value, options) {
+Jss.prototype.getDataAjax = function(attribute, options) {
     var url = JssService.getOption("url", options);
+    var jsonKey = JssService.getOption("jsonKey", options) || attribute;
 
-    console.log(url);
 
     if (typeof url === "string" && url.length >= 0) {
         var ajaxReturn = this.ajaxHelper.xhr('GET', url);
 
-        ajaxReturn.success(function(result){
+        return ajaxReturn.success(function(result){
             if (typeof result === "object") {
-                if (result[value]) {
-                    return result[value];
+                if (result[jsonKey]) {
+                    return result[jsonKey];
                 }
             }
         }).error(function(errorObject){
@@ -27,6 +31,35 @@ Jss.prototype.getDataAjax = function(value, options) {
     }
 
     return false;
+};
+
+/**
+ * -----------------------------------------------------------------------------
+ * Set data: Ajax
+ * -----------------------------------------------------------------------------
+ * Makes a post request to the options.url in a object {attribute: value}
+ *
+ * 		| Options |
+ * 		- url      {string}                                                     The request url
+ * 		- jsonKey  {string}                                                     The target key, if nested use a dot notation. If not set the attribute parameter will be used.
+ */
+Jss.prototype.setDataAjax = function(attribute, value, options) {
+    var url = JssService.getOption("url", options);
+    var requestObject = {}
+        requestObject[attribute] = value;
+
+
+    if (typeof url === "string" && url.length >= 0) {
+        var ajaxReturn = this.ajaxHelper.xhr('POST', url, requestObject);
+
+        return ajaxReturn.success(function(result){
+            return true;
+        }).error(function(errorObject){
+            console.log(errorObject);
+            return false;
+        });
+    }
+
 }
 
 

0f6fed7006f03ad3a67d823fe8379ad48c6b828e
Jeffrey Arts <jeffrey@wearereasonablepeople.com>
2015-11-15 04:13:20 +0100

    Add unit tests for the JssController + refactor of the findModules function so it can handle any domElement and it does not modifies JssController.modules but returns the found modules array instead

--- scripts/jss/jss.controller.js
+++ scripts/jss/jss.controller.js
@@ -1,10 +1,23 @@
-
+/**
+ * -------------------------------
+ * JssController is being called when the the document is ready and contains all modules which are found in the DOM.
+ * -------------------------------
+ *
+ * @type {Object}   Main object for the framework
+ */
 var  JssController = {};
-JssController.modules 		= []; // Result array with objects of all the found modules
+JssController.modules 		    = []; // Result array with objects of all the found modules
 JssController.activeModules 	= JssService.activeModules;
 
 /**
- * Rewrite this function so that you can pass in a domElement instead of it always using document. (document can be a default either way)
+ * -------------------------------
+ * Find modules
+ * -------------------------------
+ *
+ * Search within the target domElement for domElements with a classname wich equals a string in the activeModules array
+ *
+ * @param {domElement} target
+ * @return {array}                                                              Array of all the found modules in target
  */
 JssController.findModules = function(target) {
     if (typeof target !== 'object') {
@@ -14,6 +27,26 @@ JssController.findModules = function(target) {
     var allElements = target.getElementsByTagName("*");                       // Array with all domElements
     var test = [];
     var self = this;
+    var foundModules = [];
+
+
+    var addActiveModule = function(module){
+        if (JssService.isModule(element, module)) {
+            tmp = eval("new " + JssService.toCamelCase(module) + "(element)"); // Dynamicly load modules
+
+            tmp.setElement(element);                                        // Add the domElement to the module
+            tmp.findTriggers();			                                    // Search for module triggers
+
+            tmp.init();					                                    // Executes everything within the init function
+            foundModules.push(tmp);
+        }
+    };
+
+    // No child element has been found.
+    if (allElements.length <= 0) {
+        return [];
+    }
+
     for (var i=0; i < allElements.length; i++) {
 
         // Set default vars //
@@ -21,22 +54,14 @@ JssController.findModules = function(target) {
         var tmp = false;
 
         // Loop through the (active) modules array and add/instantiate them
-        self.activeModules.forEach(function(module){
-            if (JssService.isModule(element, module)) {
-                tmp = eval("new " + JssService.toCamelCase(module) + "(element)"); // Dynamicly load modules
-
-                tmp.setElement(element);                                        // Add the domElement to the module
-                tmp.findTriggers();			                                    // Search for module triggers
-
-                tmp.init();					                                    // Executes everything within the init function
-                self.modules.push(tmp);
-
-            }
-        }); // End forEach
+        self.activeModules.forEach( addActiveModule ); // End forEach
     }
+
+    // Return the amound of found modules
+    return foundModules;
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-    JssController.findModules();
+    JssController.modules = JssController.findModules();
     console.log(JssController);
 });
--- unit-tests/test.js
+++ unit-tests/test.js
@@ -1,20 +1,55 @@
 
-describe('Load all modules - Expect JssController.findModules to fill the JssController.modules array', function() {
+describe('Check vital signs of JssController', function() {
 
-    it('Expect Jss.Controller.modules to be an array', function(done){
+    it('Expect JssController.modules to be an array', function(done){
         expect(JssController.modules).toBeAn('array');
         done();
     });
 
-    it('Expect Jss.Controller.modules.length to be greater then 0', function(done){
-        expect(JssController.modules.length).toBeGreaterThan(0);
+    it('Expect JssController.activeModules to be an array', function(done){
+        expect(JssController.activeModules).toBeAn('array');
         done();
     });
 
-    var $module = 'truncate';
-    it('Expect Jss.Controller.modules to contain a property with moduleName = `' + $module + '`', function(done){
-        var test = _.find(JssController.modules, 'moduleName', $module);
-        expect(test).toBeAn('object');
+    it('Expect JssController.findModules to be a function', function(done){
+        expect(JssController.findModules).toBeA('function');
         done();
+    });
+});
+
+describe('Testing JssController.findModules() for a domElement which contains the following HTML', function() {
+    describe('<p class="truncate" data-ellepsis="..." data-lines="2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium cum exercitationem deserunt adipisci maiores nostrum laborum, saepe architecto quae eaque, natus deleniti quod blanditiis dolorum provident dolor ea corrupti.</p>', function() {
+        var dom = document.createElement('div');
+        dom.insertAdjacentHTML( 'beforeend', '<p class="truncate" data-ellepsis="..." data-lines="2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium cum exercitationem deserunt adipisci maiores nostrum laborum, saepe architecto quae eaque, natus deleniti quod blanditiis dolorum provident dolor ea corrupti.</p>' );
+
+        if (JssController.activeModules.indexOf('truncate') < 0) {
+            JssController.activeModules.push('truncate');
+        }
+
+
+
+
+        it('Expect findModules to be able to handle a domElement', function(done){
+            expect(JssController.findModules(dom)).toBeAn('array');
+            done();
+        });
+
+        it('Expect findModules to find 1 module ', function(done){
+            expect(JssController.findModules(dom).length).toBe(1);
+            done();
+        });
+
+        it('Expect findModules to find the `truncate` module ', function(done){
+            console.log(JssController.findModules(dom));
+            expect(JssController.findModules(dom)[0].moduleName).toBe('truncate');
+            done();
+        });
+        //
+        // var $module = 'truncate';
+        // it('Expect Jss.Controller.modules to contain a property with moduleName = `' + $module + '`', function(done){
+        //     var test = _.find(JssController.modules, 'moduleName', $module);
+        //     expect(test).toBeAn('object');
+        //     done();
+        // });
     });
 });
